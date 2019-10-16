0d9628308fff296f1440f0b7dbdc70d2969c0cb1
bgfist <bgfist.me@gmail.com>
2019-10-16 14:25:06 +0800

    refact: 重构request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,66 +1,73 @@
 import { wxp } from "@bgfist/weact"
 import config from "../core/config"
 import { error } from "./log"
-import { reLaunch } from "./route"
+import { reLaunch, redirectTo } from "./route"
 
 const apiError = error("API ERROR")
 
-const tokenManager = (function tokenManager() {
-  const STORE_KEY_TOKEN = "token"
-
-  return {
-    getToken() {
-      return wx.getStorageSync(STORE_KEY_TOKEN)
-    },
-
-    removeToken() {
-      return wx.removeStorageSync(STORE_KEY_TOKEN)
-    },
-
-    setToken(token: string) {
-      wx.setStorageSync(STORE_KEY_TOKEN, token)
-    }
-  }
-})()
-
 interface ApiResponse {
-  code: 200 | 401 | number
+  code: 200 | 401 | 403 | 3000 | number
   data: object
   msg: string
 }
 
-const tokenPassApis: { [api: string]: true } = {
-  "/system_key": true
-}
+export const tokenManager = (function tokenManager() {
+  const STORE_KEY_TOKEN = "token"
+  let tokenCache = ""
 
-const request = async (options: RQ<wx.RequestOption>) => {
-  let token = ""
-  if (!tokenPassApis[options.url]) {
-    token = tokenManager.getToken()
-    if (!token) {
-      return handleTokenInvalid()
+  return {
+    getToken() {
+      if (!tokenCache) {
+        tokenCache = wx.getStorageSync(STORE_KEY_TOKEN)
+      }
+      return tokenCache
+    },
+
+    removeToken() {
+      wx.removeStorageSync(STORE_KEY_TOKEN)
+      tokenCache = ""
+    },
+
+    setToken(token: string) {
+      wx.setStorageSync(STORE_KEY_TOKEN, token)
+      tokenCache = token
     }
   }
+})()
 
-  options = {
-    ...options,
-    url: config.baseUrl + options.url,
+type RequestOptions = RQ<wx.RequestOption> & { baseUrl?: string; auth?: boolean }
+
+const req = async (options: RequestOptions) => {
+  const { baseUrl = config.baseUrl, url, auth = true, ...otherOptions } = options
+
+  const token = tokenManager.getToken()
+
+  if (auth && !token) {
+    goLogin()
+    throw new Error("需要登录")
+  }
+
+  const reqOptions = {
+    ...otherOptions,
+    url: baseUrl + url,
     header: {
-      token,
-      ...options.header
+      token: auth ? token : undefined,
+      ...otherOptions.header
     }
   }
 
-  // if (config.apiMode === "mock") {
-  //   return wxp.request(options) as Promise<any>
-  // }
+  if (config.apiMode === "mock") {
+    return wxp.request(reqOptions) as Promise<any>
+  }
 
-  const res = await wxp.request(options).catch(err => {
+  const res = await wxp.request(reqOptions).catch(err => {
     wx.showToast({
       title: "网络错误",
       icon: "none"
     })
+
     apiError("request报错", err)
+
     throw new Error(err)
   })
 
@@ -74,26 +81,30 @@ const handleApiResponse = (res: any) => {
   }
 
   if (code === 401) {
-    return handleTokenInvalid()
+    tokenManager.removeToken()
+    goLogin()
+    throw new Error("token失效")
+  }
+
+  if (code === 3000) {
+    redirectTo("error", { error: msg })
+    throw new Error("没有权限访问")
   }
 
   wx.showToast({
     title: msg,
     icon: "none"
   })
+
   apiError(msg)
+
   throw new Error(msg)
 }
 
-const handleTokenInvalid = () => {
-  tokenManager.removeToken()
-  reLaunch("login")
-  throw new Error("token失效")
-}
+export const goLogin = () => reLaunch("welcome")
 
-type RequestOption = Omit<RQ<wx.RequestOption>, "method">
+type RequestOption = Omit<RequestOptions, "method">
 
-export const saveToken = tokenManager.setToken
-export const get = (options: RequestOption) => request({ method: "GET", ...options })
-export const post = (options: RequestOption) => request({ method: "POST", ...options })
-export const put = (options: RequestOption) => request({ method: "PUT", ...options })
+export const get = (options: RequestOption) => req({ method: "GET", ...options })
+export const post = (options: RequestOption) => req({ method: "POST", ...options })
+export const put = (options: RequestOption) => req({ method: "PUT", ...options })
